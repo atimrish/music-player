966e878796742ea10cd1510393dd8fe2c816f589
atimrish <atimrish11@gmail.com>
2024-10-28 22:27:10 +0400

    коммит для инициализации

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native'
+import {StatusBar} from "expo-status-bar";
+
+export default function App() {
+    return (
+        <View>
+            <StatusBar style="auto"/>
+            <Text>Тест</Text>
+        </View>
+    );
+}
